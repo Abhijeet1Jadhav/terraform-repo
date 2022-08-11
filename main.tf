baec76947bc348f1977eccf5ca04309005bedcbe
root <root@ip-172-31-45-22.ap-south-1.compute.internal>
2022-08-11 06:39:26 +0000

    v1

---
+++ main.tf
@@ -0,0 +1,9 @@
+provide "aws" {
+ access_key = "AKIA6OEYWF3UTM6XG4J4"
+ secret_key = "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"
+}
+
+resource "aws_instance" "webserver1" {
+ami = "ami-07eaf27c7c4a884cf"
+instance_type = "t2.micro"
+}
