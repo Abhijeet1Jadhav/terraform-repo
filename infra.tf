209cfc7027a894efff2289707481c844fd596b01
root <root@ip-172-31-10-149.ap-south-1.compute.internal>
2022-08-16 18:58:54 +0000

    final

--- infra.tf
+++ infra.tf
@@ -1,9 +1,12 @@
 provider "aws" {}
 
 resource "aws_instance" "set1" {
+ id = "i-0a379333074222db3"
  ami = "ami-08df646e18b182346"
- instance_type = "t3.large"
+ instance_type = "t2.micro"
  tags = {
   Name = "Test1"
+  owner = "Abhijeet"
 	}
+ security_groups = "sg-0be2426bd6b3f0ee0"
 }
