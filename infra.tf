63fe19c0d3fb3db7d863baa5a20c866e6a6aa4cf
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2022-08-17 00:38:02 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -1,12 +1,10 @@
 provider "aws" {}
 
 resource "aws_instance" "set1" {
- id = "i-0a379333074222db3"
  ami = "ami-08df646e18b182346"
  instance_type = "t2.micro"
  tags = {
   Name = "Test1"
   owner = "Abhijeet"
-	}
- security_groups = "launch-wizard-1"
+ }
 }
