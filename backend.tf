81f656859d8eb6d8fadd31e73216cd0b98a6fd36
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2024-01-15 10:40:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ab-demo1"
-    key            = "${terraform.workspace}/terraform.tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "us-east-2"
   }
 }
