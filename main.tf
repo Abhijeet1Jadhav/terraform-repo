b2307c2ee6304c59e6b69da7b84ae943167748a7
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2024-01-15 08:17:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}     
+
+#terraform { cloud { organization = "jadhavabhijeet640" workspaces { name = "demo-workspace" } } }
+
+provider "aws" {
+  access_key = "AKIAVYQ4LEAU7OE2AXDV"
+  secret_key = "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"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "demo" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+}
+variable "instance_type" {}
+variable "ami_id" {}
+
+output "arn" {
+  value = aws_instance.demo.arn
+}
+
+output "public_ip" {
+  value = aws_instance.demo.public_ip
+}
